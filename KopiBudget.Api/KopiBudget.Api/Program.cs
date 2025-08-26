236705064cf1bb4ad670de944f55654146d3b9de
Code Coffee <code.coffee.2024@gmail.com>
2025-08-26 23:11:22 +0800

    Update program

--- KopiBudget.Api/KopiBudget.Api/Program.cs
+++ KopiBudget.Api/KopiBudget.Api/Program.cs
@@ -92,11 +92,9 @@ builder.Services.AddSwaggerGen(c =>
 });
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
