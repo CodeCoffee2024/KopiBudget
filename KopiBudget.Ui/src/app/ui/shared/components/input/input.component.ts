f64529ab9167db18057aadaec661e3c8411f4012
Code Coffee <code.coffee.2024@gmail.com>
2025-08-31 00:21:35 +0800

    UI - Fix input behavior

--- KopiBudget.Ui/src/app/ui/shared/components/input/input.component.ts
+++ KopiBudget.Ui/src/app/ui/shared/components/input/input.component.ts
@@ -10,9 +10,7 @@ import { InputTypes } from '../../../../domain/models/input-type';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent
-	implements ControlValueAccessor
-{
+export class InputComponent implements ControlValueAccessor {
 	@Input() inputType: InputTypes = InputTypes.Text;
 	@Input() label: string = '';
 	@Input() hideLabel = false;
@@ -22,101 +20,86 @@ export class InputComponent
 	@Input() formGroup!: FormGroup;
 
 	isPasswordVisible = false;
+
 	get formControl(): FormControl {
-		return this.formGroup?.get(
-			this.controlName
-		) as FormControl;
+		return this.formGroup?.get(this.controlName) as FormControl;
 	}
 
 	togglePasswordVisibility(): void {
 		this.isPasswordVisible = !this.isPasswordVisible;
 	}
+
 	get errors(): string[] {
-		if (!this.formControl || !this.formControl.errors)
-			return [];
+		if (!this.formControl || !this.formControl.errors || !this.formControl.touched) return [];
 
 		const errors = this.formControl.errors;
 		const errorMessages: string[] = [];
 
 		for (const errorKey of Object.keys(errors)) {
-			const value = errors[errorKey];
-			if (errorKey === 'required') {
-				errorMessages.push(`${this.label} is required`);
-			} else if (errorKey === 'email') {
-				errorMessages.push(
-					`${this.label} must be a valid email`
-				);
-			} else if (errorKey === 'mustMatch') {
-				errorMessages.push(
-					`${this.label} must match ${value}`
-				);
-			} else if (errorKey === 'minlength') {
-				const min = value?.requiredLength;
-				errorMessages.push(
-					`${this.label} must be at least ${min} characters`
-				);
-			} else if (errorKey === 'serverError') {
-				if (Array.isArray(value)) {
-					value.forEach((msg: string) =>
-						errorMessages.push(msg)
-					);
-				} else {
-					errorMessages.push(value);
-				}
+		const value = errors[errorKey];
+		if (errorKey === 'required') {
+			errorMessages.push(`${this.label} is required`);
+		} else if (errorKey === 'email') {
+			errorMessages.push(`${this.label} must be a valid email`);
+		} else if (errorKey === 'mustMatch') {
+			errorMessages.push(`${this.label} must match ${value}`);
+		} else if (errorKey === 'minlength') {
+			const min = value?.requiredLength;
+			errorMessages.push(`${this.label} must be at least ${min} characters`);
+		} else if (errorKey === 'serverError') {
+			if (Array.isArray(value)) {
+			value.forEach((msg: string) => errorMessages.push(msg));
 			} else {
-				errorMessages.push(
-					`${this.label} ${errorKey
-						.replace(/([A-Z])/g, ' $1')
-						.toLowerCase()}`
-				);
+			errorMessages.push(value);
 			}
+		} else {
+			errorMessages.push(
+			`${this.label} ${errorKey.replace(/([A-Z])/g, ' $1').toLowerCase()}`
+			);
+		}
 		}
 
 		return errorMessages;
 	}
+
 	get isRequired(): boolean {
 		return !!this.formControl?.errors?.['required'];
 	}
+
 	onInput(event: Event): void {
 		const input = event.target as HTMLInputElement;
+
 		if (this.inputType === InputTypes.Month) {
-			let value = this.formControl.value;
-			if (value && /^\d{4}-\d{2}$/.test(value)) {
-				this.formControl.setValue(value, {
-					emitEvent: false,
-				});
-			} else {
+			const value = input.value;
+			if (!/^\d{4}-\d{2}$/.test(value)) {
 				this.formControl.setValue('', { emitEvent: false });
 			}
 		}
+
 		if (this.inputType === 'number') {
 			const regex = /^\d*(\.\d{0,2})?$/;
 			if (!regex.test(input.value)) {
 				input.value = input.value.slice(0, -1);
-				this.formControl.setValue(input.value, {
-					emitEvent: false,
-				});
+				this.formControl.setValue(input.value, { emitEvent: false });
 			}
 		}
 	}
 
 	onBlur(): void {
+		this.formControl.markAsTouched(); // ✅ only mark THIS field touched
+		console.log(this.formControl);
 		if (this.inputType === 'number') {
 			let value = parseFloat(this.formControl.value);
 			if (!isNaN(value)) {
 				value = parseFloat(value.toFixed(2));
-				this.formControl.setValue(value, {
-					emitEvent: false,
-				});
+				this.formControl.setValue(value, { emitEvent: false });
 			}
 		}
 	}
 
 	writeValue(value: any): void {
 		if (this.formControl) {
-			this.formControl.setValue(value, {
-				emitEvent: false,
-			});
+		this.formControl.setValue(value, { emitEvent: false });
 		}
 	}
 
@@ -124,7 +107,9 @@ export class InputComponent
 		this.formControl?.valueChanges.subscribe(fn);
 	}
 
-	registerOnTouched(): void {}
+	registerOnTouched(fn: any): void {
+		this.formControl?.registerOnChange(fn);
+	}
 
 	setDisabledState(isDisabled: boolean): void {
 		if (isDisabled) {
